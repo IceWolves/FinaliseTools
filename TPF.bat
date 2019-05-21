2446d4de4eb30dd8092fd58795f7943f04165be5
IceWolves <50860473+IceWolves@users.noreply.github.com>
2019-05-21 14:34:28 +0200

    Kommentare hinzugefügt

--- TPF.bat
+++ TPF.bat
@@ -1,4 +1,11 @@
-@echo off
+@echo off ::Ausschalten der Anzeige von Befehlen in der CMD
+
+::Anzahl der Dokumente die finalisiert werden sollen
+set /a anzahlPDF=0
+echo %anzahlPDF% zu verschiebende/s PDF
+
+
+::Nachfragen ob die Dokumente wirklich finalisiert werden sollen
 :NotYorN
 set /p askIfSure="Wollen sie die TST PDFs in ihrem Downloadordner finalisieren(y/n)? "
 if "%askIfSure%" == "n" goto ENDwoERR
@@ -6,10 +13,13 @@ if "%askIfSure%" == "y" goto FINALISE
 
 goto NotYorN
 
+::Finalisierung der Dokumente
 :FINALISE
 set /p kw="In welches KW sollen die PDFs verschoben werden? "
-echo "J:/02_Service Projects/SrvProjects_DE/apoBank/Korrespondenz/allgemein/Auslieferung/KW%kw%"
 
+echo J:/02_Service Projects/SrvProjects_DE/apoBank/Korrespondenz/allgemein/Auslieferung/KW%kw%
+
+::Programmende
 :ENDwoERR
 echo Programm beendet
 ::exit 0
