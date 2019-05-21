414226063145630bc4466df7658f2ffad4393046
IceWolves <50860473+IceWolves@users.noreply.github.com>
2019-05-21 14:25:46 +0200

    Weiterentwicklung an der TPF.bat Datei

--- TPF.bat
+++ TPF.bat
@@ -1,6 +1,15 @@
 @echo off
-set /p askIfSure=Wollen sie die TST PDFs in ihrem Downloadordner finalisieren(y/n)?
-::notYorN
-if "%askIfSure%" == "n" echo Programm wird beendet.
-    ::exit 0
-if "%askIfSure%" == "y" set /p kw=Geben Sie die KW Nummer der Dokumente ein:
+:NotYorN
+set /p askIfSure="Wollen sie die TST PDFs in ihrem Downloadordner finalisieren(y/n)? "
+if "%askIfSure%" == "n" goto ENDwoERR
+if "%askIfSure%" == "y" goto FINALISE
+
+goto NotYorN
+
+:FINALISE
+set /p kw="In welches KW sollen die PDFs verschoben werden? "
+echo "J:/02_Service Projects/SrvProjects_DE/apoBank/Korrespondenz/allgemein/Auslieferung/KW%kw%"
+
+:ENDwoERR
+echo Programm beendet
+::exit 0
